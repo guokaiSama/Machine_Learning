882a8279a966e3759f962c369be5e74f69e13eb2
guokaiSama <gk605665991@163.com>
2018-12-29 17:07:15 +0800

    updata

--- logisticRegress/begin.py
+++ logisticRegress/begin.py
@@ -7,10 +7,13 @@ sklearn的逻辑回归接口使用
 by:guoKaiSama
 """
 import logisticRegress
+import sklearnLogistic
 if __name__=="__main__":
-    print "%04d"%3
-
     # 测试逻辑回归与梯度上升
     #logisticRegress.testLogisticRegress()
     #logisticRegress.fitLogistic()
+
+    # sklearn的实现
+    sklearnLogistic.sklearnTest()
+    #sklearnLogistic.func()
 
--- logisticRegress/logisticRegress.py
+++ logisticRegress/logisticRegress.py
@@ -3,7 +3,6 @@
 import matplotlib.pylab as plt
 import numpy as np
 import pandas as pd
-from numpy import array,mat,shape,ones
 from sklearn.model_selection import train_test_split
 # 解析以tab键分隔的文件中的浮点数
 def loadDataSet(fileName):
@@ -14,21 +13,17 @@ def loadDataSet(fileName):
     """
     dataMat = []
     labelMat = []
-    f = open(fileName, 'r')
-    for line in f.readlines():
-        line_arr = line.strip().split()
-        # 为了方便计算，我们将 X0 的值设为 1.0 ，也就是在每一行的开头添加一个 1.0 作为 X0
-        dataMat.append([1.0, np.float(line_arr[0]), np.float(line_arr[1])])
-        labelMat.append(int(line_arr[2]))
+    with open(fileName, 'r') as f:
+        for line in f.readlines():
+            line_arr = line.strip().split()
+            # 为了方便计算，我们将 X0 的值设为 1.0 ，也就是在每一行的开头添加一个 1.0 作为 X0
+            dataMat.append([1.0, np.float(line_arr[0]), np.float(line_arr[1])])
+            labelMat.append(int(line_arr[2]))
     return dataMat, labelMat
 
 
 # sigmod函数
 def sigmoid(x):
-    # 这里其实非常有必要解释一下，会出现的错误 RuntimeWarning: overflow encountered in exp
-    # 这里是因为我们输入的有的 x 实在是太小了，比如 -6000之类的，那么计算一个数字 np.exp(6000)这个结果太大了，没法表示，所以就溢出了
-    # 如果是计算 np.exp（-6000），这样虽然也会溢出，但是这是下溢，就是表示成零
-    # 去网上搜了很多方法，比如 使用bigfloat这个库（我竟然没有安装成功，就不尝试了，反正应该是有用的
     return 1.0 / (1 + np.exp(-x))
 
 # 画出数据集和 Logistic 回归最佳拟合直线的函数
@@ -38,36 +33,33 @@ def plotBestFit(dataArr, labelMat, weights):
     labelMat:样本数据的类别标签，即目标变量
     weights:回归系数
     '''
-    n = shape(dataArr)[0]
-    xcord1 = []
-    ycord1 = []
-    xcord2 = []
-    ycord2 = []
+    n = np.shape(dataArr)[0]
+    x1Lable0 = []
+    x2Lable0 = []
+    x1Lable1 = []
+    x2Lable1 = []
 
     for i in range(n):
         if int(labelMat[i])== 1:
-            xcord1.append(dataArr[i,1])
-            ycord1.append(dataArr[i,2])
+            x1Lable0.append(dataArr[i,1])
+            x2Lable0.append(dataArr[i,2])
         else:
-            xcord2.append(dataArr[i,1])
-            ycord2.append(dataArr[i,2])
+            x1Lable1.append(dataArr[i,1])
+            x2Lable1.append(dataArr[i,2])
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
-    ax.scatter(xcord2, ycord2, s=30, c='green')
+    ax.scatter(x1Lable0, x2Lable0, s=30, c='red', marker='s')
+    ax.scatter(x1Lable1, x2Lable1, s=30, c='green')
     x = np.arange(-3.0, 3.0, 0.1)
     """
-    y的由来：
-    首先理论上是这个样子的。
-    dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
-    f(x) = w0*x0+w1*x1+w2*x2
-    x0最开始就设置为1， x2就是我们画图的y值，而f(x)被我们磨合误差给算到w0,w1,w2身上去了
-    所以： w0+w1*x+w2*y=0 => y = (-w0-w1*x)/w2   
+    f(x) = w0+w1*x1+w2*x2
+    x1对应x坐标，x2对应y坐标，而f(x)被磨合误差给算到了w0,w1,w2上
     """
     y = (-weights[0]-weights[1]*x)/weights[2]
     ax.plot(x, y)
-    plt.xlabel('X'); plt.ylabel('Y')
+    plt.xlabel('X')
+    plt.ylabel('Y')
     plt.show()
 
 
@@ -75,20 +67,17 @@ def plotBestFit(dataArr, labelMat, weights):
 # 找到 Logistic 分类器在此数据集上的最佳回归系数
 def gradAscent(dataMatIn, classLabels):
     """
-    :param dataMatIn:
-        dataMatIn 是一个2维NumPy数组，每列分别代表每个不同的特征，每行则代表每个训练样本
-    :param classLabels:
-        classLabels 是类别标签，它是一个 1*100 的行向量。为了便于矩阵计算，需要将该行向量转换为列向量，做法是将原向量转置，再将它赋值给labelMat。
-    :return:
+    ataMatIn 样本取值
+    classLabels 是类别标签
     """
     # 转化为矩阵[[1,1,2],[1,1,2]....]
-    dataMatrix = mat(dataMatIn)
+    dataMatrix = np.mat(dataMatIn)
 
     # 转化为矩阵[[0,1,0,1,0,1.....]]，并转置[[0],[1],[0].....]
-    labelMat = mat(classLabels).transpose()
+    labelMat = np.mat(classLabels).transpose()
 
-    # m->数据量，样本数 n->特征数
-    m,n = shape(dataMatrix)
+    # m:样本数 n:特征数
+    m,n = np.shape(dataMatrix)
 
     # alpha代表向目标移动的步长
     alpha = 0.001
@@ -96,22 +85,14 @@ def gradAscent(dataMatIn, classLabels):
     # 迭代次数
     maxCycles = 500
 
-    # weights 代表回归系数， 此处的 ones((n,1)) 创建一个长度和特征数相同的矩阵，值全部都是 1
-    weights = ones((n,1))
+    # 初始化 weights， n个长度为1的向量，值均为1
+    weights = np.ones((n,1))
 
     for k in range(maxCycles):
-        # m*3 的矩阵 * 3*1 的单位矩阵 ＝ m*1的矩阵
-        # 那么乘上单位矩阵的意义，就代表：通过公式得到的理论值
-        # 矩阵乘法
         h = sigmoid(dataMatrix*weights)
-
-        # labelMat是实际值
-        # 向量相减
         error = (labelMat - h)
-
-        # 0.001* (3*m)*(m*1) 表示在每一个列上的一个误差情况，最后得出 x1,x2,xn的系数的偏移量
         weights = weights + alpha * dataMatrix.transpose() * error
-    return array(weights)
+    return np.array(weights)
 
 
 # 随机梯度上升
@@ -223,10 +204,10 @@ def fitLogistic():
 
 def testLogisticRegress():
     # 加载数据
-    dataMat, labelMat = loadDataSet("./data/TestSet.txt")
+    dataMat, labelMat = loadDataSet("./data/logisticRegressTestData.txt")
 
-    # 2.训练模型，因为数组没有是复制n份， array的乘法就是乘法
-    dataArr = array(dataMat)
+    # 训练模型
+    dataArr = np.array(dataMat)
     #weights = gradAscent(dataArr, labelMat)
 
     # 优化后的随机梯度上升
