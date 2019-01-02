fa1f4059f02ac8e05726f48bd5cab1da5dbef13c
guokaiSama <gk605665991@163.com>
2019-01-02 19:31:53 +0800

    updata

--- SVD/begin.py
+++ SVD/begin.py
@@ -4,13 +4,12 @@
 SVD算法的具体应用
 by:guoKaiSama
 """
-
+import recomSVD
 if __name__=="__main__":
+    # 使用sklearn对矩阵进行SVD分解
+    #recomSVD.sklearnSVD()
     # 基于协同过滤的推荐系统
-    #logisticRegress.testLogisticRegress()
-    #logisticRegress.fitLogistic()
-
-    # sklearn的实现
-    sklearnLogistic.sklearnTest()
-    #sklearnLogistic.func()
+    #recomSVD.recommendTest()
+    # 压缩图片,允许基于任意给定的奇异值数目来重构图像
+    print recomSVD.imgCompress(2)
 
--- SVD/recomSVD.py
+++ SVD/recomSVD.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # coding:utf-8
+
 """
 协同过滤：是通过将用户和其他用户的数据进行对比来实现推荐的。
 当知道了两个用户或两个物品之间的相似度，我们就可以利用已有的数据来预测未知用户的喜好。
@@ -31,52 +32,21 @@
 2）在实际中，另一个普遍的做法就是离线计算并保存相似度得分。(物品相似度可能被用户重复的调用)
 3）冷启动问题，解决方案就是将推荐看成是搜索问题，通过各种标签／属性特征进行基于内容的推荐。
 """
+import numpy as np
 def loadExData():
-    """
-    # 推荐引擎示例矩阵
-    return[[4, 4, 0, 2, 2],
-           [4, 0, 0, 3, 3],
-           [4, 0, 0, 1, 1],
-           [1, 1, 1, 2, 0],
-           [2, 2, 2, 0, 0],
-           [1, 1, 1, 0, 0],
-           [5, 5, 5, 0, 0]]
-    """
-    # # 原矩阵
-    # return[[1, 1, 1, 0, 0],
-    #        [2, 2, 2, 0, 0],
-    #        [1, 1, 1, 0, 0],
-    #        [5, 5, 5, 0, 0],
-    #        [1, 1, 0, 2, 2],
-    #        [0, 0, 0, 3, 3],
-    #        [0, 0, 0, 1, 1]]
-
-    # 原矩阵
+    # 原始矩阵
     return[[0, -1.6, 0.6],
            [0, 1.2, 0.8],
            [0, 0, 0],
            [0, 0, 0]]
-def loadExData2():
-    # 书上代码给的示例矩阵
-    return[[0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 5],
-           [0, 0, 0, 3, 0, 4, 0, 0, 0, 0, 3],
-           [0, 0, 0, 0, 4, 0, 0, 1, 0, 4, 0],
-           [3, 3, 4, 0, 0, 0, 0, 2, 2, 0, 0],
-           [5, 4, 5, 0, 0, 0, 0, 5, 5, 0, 0],
-           [0, 0, 0, 0, 5, 0, 1, 0, 0, 5, 0],
-           [4, 3, 4, 0, 0, 0, 0, 5, 5, 0, 1],
-           [0, 0, 0, 4, 0, 4, 0, 0, 0, 0, 4],
-           [0, 0, 0, 2, 0, 2, 5, 0, 0, 1, 2],
-           [0, 0, 0, 0, 5, 0, 0, 0, 0, 4, 0],
-           [1, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0]]
-def loadExData3():
+def recommendData():
     # 利用SVD提高推荐效果，菜肴矩阵
     """
     行：代表人
     列：代表菜肴名词
     值：代表人对菜肴的评分，0表示未评分
     """
-    return [[2, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0],
+    return np.mat([[2, 0, 0, 4, 4, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5],
             [0, 0, 0, 0, 0, 0, 0, 1, 0, 4, 0],
             [3, 3, 4, 0, 3, 0, 0, 2, 2, 0, 0],
@@ -86,7 +56,8 @@ def loadExData3():
             [0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 4],
             [0, 0, 0, 0, 0, 0, 5, 0, 0, 5, 0],
             [0, 0, 0, 3, 0, 0, 0, 0, 4, 5, 0],
-            [1, 1, 2, 1, 1, 2, 1, 0, 4, 5, 0]]
+            [1, 1, 2, 1, 1, 2, 1, 0, 4, 5, 0]])
+
 # 相似度计算，假定inA和inB 都是列向量
 # 基于欧氏距离
 def ecludSim(inA, inB):
@@ -99,13 +70,13 @@ def pearsSim(inA, inB):
     # 如果不存在，该函数返回1.0，此时两个向量完全相关。
     if len(inA) < 3:
         return 1.0
-    return 0.5 + 0.5 * corrcoef(inA, inB, rowvar=0)[0][1]
+    return 0.5 + 0.5 * np.corrcoef(inA, inB, rowvar=0)[0][1]
 
 
 # 计算余弦相似度，如果夹角为90度，相似度为0；如果两个向量的方向相同，相似度为1.0
 def cosSim(inA, inB):
     num = float(inA.T*inB)
-    denom = la.norm(inA)*la.norm(inB)
+    denom = np.linalg.norm(inA)*np.linalg.norm(inB)
     return 0.5 + 0.5*(num/denom)
 
 # 基于物品相似度的推荐引擎
@@ -115,9 +86,8 @@ def cosSim(inA, inB):
 （3）求用户集合的交集，计算欧氏距离
 （4）当新来用户评论过wupini，那么我们就可以用相似度预测该用户对物品j的评分
 """
-def standEststandEst(dataMat, user, simMeas, item):
-    """standEst(计算某用户未评分物品中，以对该物品和其他物品评分的用户的物品相似度，然后进行综合评分)
-
+def standEst(dataMat, user, simMeas, item):
+    """
     Args:
         dataMat         训练数据集
         user            用户编号
@@ -131,32 +101,34 @@ def standEststandEst(dataMat, user, simMeas, item):
     # 初始化两个评分值
     simTotal = 0.0
     ratSimTotal = 0.0
+
     # 遍历行中的每个物品（对用户评过分的物品进行遍历，并将它与其他物品进行比较）
     for j in range(n):
         userRating = dataMat[user, j]
         # 如果某个物品的评分值为0，则跳过这个物品
         if userRating == 0:
             continue
-        # 寻找两个用户都评级的物品
-        # 变量 overLap 给出的是两个物品当中已经被评分的那个元素的索引ID
-        # logical_and 计算x1和x2元素的真值。
-        overLap = nonzero(logical_and(dataMat[:, item].A > 0, dataMat[:, j].A > 0))[0]
+
+        #找到item和j均不为0（有评分）的用户
+        overLap = np.nonzero(np.logical_and(dataMat[:, item].A > 0, dataMat[:, j].A > 0))[0]
         # 如果相似度为0，则两着没有任何重合元素，终止本次循环
         if len(overLap) == 0:
             similarity = 0
-        # 如果存在重合的物品，则基于这些重合物重新计算相似度。
         else:
+            # 如果存在重合的物品，则基于这些重合物重新计算相似度。
             similarity = simMeas(dataMat[overLap, item], dataMat[overLap, j])
-        # print 'the %d and %d similarity is : %f'(iten,j,similarity)
+
         # 相似度会不断累加，每次计算时还考虑相似度和当前用户评分的乘积
         # similarity  用户相似度，   userRating 用户评分
         simTotal += similarity
         ratSimTotal += similarity * userRating
     if simTotal == 0:
         return 0
+
     # 通过除以所有的评分总和，对上述相似度评分的乘积进行归一化，使得最后评分在0~5之间，这些评分用来对预测值进行排序
     else:
         return ratSimTotal/simTotal
+
 # 基于SVD的评分估计
 # 在recommend() 中，这个函数用于替换对standEst()的调用，该函数对给定用户给定物品构建了一个评分估计值
 def svdEst(dataMat, user, simMeas, item):
@@ -171,19 +143,17 @@ def svdEst(dataMat, user, simMeas, item):
         ratSimTotal/simTotal     评分（0～5之间的值）
     """
     # 物品数目
-    n = shape(dataMat)[1]
+    n = np.shape(dataMat)[1]
     # 对数据集进行SVD分解
     simTotal = 0.0
     ratSimTotal = 0.0
-    # 奇异值分解
-    # 在SVD分解之后，我们只利用包含了90%能量值的奇异值，这些奇异值会以NumPy数组的形式得以保存
-    U, Sigma, VT = la.svd(dataMat)
 
-    # # 分析 Sigma 的长度取值
-    # analyse_data(Sigma, 20)
+    # 在SVD分解之后，我们只利用包含了90%能量值的奇异值，这些奇异值会以NumPy数组的形式得以保存
+    U, Sigma, VT = np.linalg.svd(dataMat)
 
     # 如果要进行矩阵运算，就必须要用这些奇异值构建出一个对角矩阵
-    Sig4 = mat(eye(4) * Sigma[: 4])
+    Sig4 = np.mat(np.eye(4) * Sigma[: 4])
+
     # 利用U矩阵将物品转换到低维空间中，构建转换后的物品(物品+4个主要的特征)
     xformedItems = dataMat.T * U[:, :4] * Sig4.I
     # 对于给定的用户，for循环在用户对应行的元素上进行遍历，
@@ -205,15 +175,18 @@ def svdEst(dataMat, user, simMeas, item):
     else:
         # 计算估计评分
         return ratSimTotal / simTotal
-# recommend()函数，就是推荐引擎，它默认调用standEst()函数，产生了最高的N个推荐结果。
-# 如果不指定N的大小，则默认值为3。该函数另外的参数还包括相似度计算方法和估计方法
+
+# N=3表示输出top3个推荐结果
 def recommend(dataMat, user, N=3, simMeas=cosSim, estMethod=standEst):
-    # 寻找未评级的物品
-    # 对给定的用户建立一个未评分的物品列表
-    unratedItems = nonzero(dataMat[user, :].A == 0)[1]
+    # 每行一个用户，user用来表示用户所在的行数
+    # .A表示将matrix的数组转换成ndarray
+    # unratedItems所有没有评分的菜品的下标
+    unratedItems = np.nonzero(dataMat[user, :].A == 0)[1]
+
     # 如果不存在未评分物品，那么就退出函数
     if len(unratedItems) == 0:
         return 'you rated everything'
+
     # 物品的编号和评分值
     itemScores = []
     # 在未评分物品上进行循环
@@ -221,8 +194,44 @@ def recommend(dataMat, user, N=3, simMeas=cosSim, estMethod=standEst):
         estimatedScore = estMethod(dataMat, user, simMeas, item)
         # 寻找前N个未评级物品，调用standEst()来产生该物品的预测得分，该物品的编号和估计值会放在一个元素列表itemScores中
         itemScores.append((item, estimatedScore))
-        # 按照估计得分，对该列表进行排序并返回。列表逆排序，第一个值就是最大值
+
+    # 按照估计得分，对该列表进行排序并返回。列表逆排序，第一个值就是最大值
     return sorted(itemScores, key=lambda jj: jj[1], reverse=True)[: N]
+
+# 图像压缩函数
+# 加载并转换数据
+def imgLoadData(filename):
+    myl = []
+    # 打开文本文件，并从文件以数组方式读入字符
+    for line in open(filename).readlines():
+        newRow = []
+        for i in range(32):
+            newRow.append(int(line[i]))
+        myl.append(newRow)
+    # 矩阵调入后，就可以在屏幕上输出该矩阵
+    myMat = np.mat(myl)
+    return myMat
+
+
+# 实现图像压缩，允许基于任意给定的奇异值数目来重构图像
+def imgCompress(numSV=3, thresh=0.8):
+    """imgCompress( )
+    Args:
+        numSV       Sigma长度
+        thresh      判断的阈值
+    """
+    # 构建一个列表
+    myMat = imgLoadData('./data/imageData.txt')
+
+    # Sigma是一个对角矩阵，因此需要建立一个全0矩阵，然后将前面的那些奇异值填充到对角线上。
+    U, Sigma, VT = np.linalg.svd(myMat)
+
+    # 分析插入的 Sigma 长度
+    analyse_data(Sigma, 20)
+
+    SigRecon = np.mat(np.eye(numSV) * Sigma[: numSV])
+    reconMat = U[:, :numSV] * SigRecon * VT[:numSV, :]
+    return reconMat
 
 
 def analyse_data(Sigma, loopNum=20):
@@ -242,44 +251,23 @@ def analyse_data(Sigma, loopNum=20):
         '''
         print('主成分：%s, 方差占比：%s%%' % (format(i + 1, '2.0f'), format(SigmaI / SigmaSum * 100, '4.2f')))
 
-
-if __name__ == "__main__":
-    # # 对矩阵进行SVD分解(用python实现SVD)
-    # Data = loadExData()
-    # print('Data:', Data)
-    # U, Sigma, VT = linalg.svd(Data)
-    # # 打印Sigma的结果，因为前3个数值比其他的值大了很多，为9.72140007e+00，5.29397912e+00，6.84226362e-01
-    # # 后两个值比较小，每台机器输出结果可能有不同可以将这两个值去掉
-    # print('U:', U)
-    # print('Sigma', Sigma)
-    # print('VT:', VT)
-    # print('VT:', VT.T)
-
-    # # 重构一个3x3的矩阵Sig3
-    # Sig3 = mat([[Sigma[0], 0, 0], [0, Sigma[1], 0], [0, 0, Sigma[2]]])
-    # print(U[:, :3] * Sig3 * VT[:3, :])
-
-    """
-    # 计算欧氏距离
-    myMat = mat(loadExData())
-    # print(myMat)
-    print(ecludSim(myMat[:, 0], myMat[:, 4]))
-    print(ecludSim(myMat[:, 0], myMat[:, 0]))
-    # 计算余弦相似度
-    print(cosSim(myMat[:, 0], myMat[:, 4]))
-    print(cosSim(myMat[:, 0], myMat[:, 0]))
-    # 计算皮尔逊相关系数
-    print(pearsSim(myMat[:, 0], myMat[:, 4]))
-    print(pearsSim(myMat[:, 0], myMat[:, 0]))
-    """
-
+def recommendTest():
     # 计算相似度的方法
-    myMat = mat(loadExData3())
-    # print(myMat)
-    # 计算相似度的第一种方式
-    print(recommend(myMat, 1, estMethod=svdEst))
-    # 计算相似度的第二种方式
+    myMat = recommendData()
+    print recommend(myMat, 1)
+    print recommend(myMat, 1, estMethod=svdEst)
     print(recommend(myMat, 1, estMethod=svdEst, simMeas=pearsSim))
 
-    # 默认推荐（菜馆菜肴推荐示例）
-    print(recommend(myMat, 2))
+def sklearnSVD():
+    # 使用sklearn对矩阵进行SVD分解
+    Data = loadExData()
+    print('Data:', Data)
+    U, Sigma, VT = np.linalg.svd(Data)
+
+    print('U:', U)
+    print('Sigma', Sigma)
+    print('VT:', VT)
+
+    # 重构一个3x3的矩阵Sig3
+    Sig3 = np.mat([[Sigma[0], 0, 0], [0, Sigma[1], 0], [0, 0, Sigma[2]]])
+    print(U[:, :3] * Sig3 * VT[:3, :])
